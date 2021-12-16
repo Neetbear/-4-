28688b1ef2fc43f1d4c46fbef3daed03494ec4b4
Neetbear <flffkry@gmail.com>
2021-12-17 08:51:14 +0900

    1217

--- react_work/homework/homwork16/ArchangelC.js
+++ react_work/homework/homwork16/ArchangelC.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 class Gundam extends React.Component {
-    render() {
-        const gundams = [
+    constructor(props) {
+        super(props);
+        this.state = [
             {id:1, model:"ZGMF-X10A Freedom Gundam"},
             {id:2, model:"ZGMF-X109 Justice Gundam"},
             {id:3, model:"Sword Strike Gundam"}
-        ]
-        const gundamList = gundams.map((gundam) => <li>This is {gundam.id + " " +gundam.model}</li>)
+        ];
+    }
+    render() {
+        const gundamList = this.state.map((mobile) => <li>This is {mobile.id + " " +mobile.model}</li>)
         return (
             <>{gundamList}</>
         );
