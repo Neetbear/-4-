13082a320e1523d5215663c4cc73c0597381d326
Neetbear <flffkry@gmail.com>
2021-12-29 14:14:04 +0900

    1229

--- react_work/test/src/components/Signin.js
+++ react_work/test/src/components/Signin.js
@@ -3,8 +3,27 @@ import React from "react";
 function Signin({}) {
 
     return(
-        <div>
-        </div>
+        <form action='signin'>
+            <input type="text" className="userid" placeholder="ID를 입력하세요" require />
+            <br />
+            <input type="password" className="userpassword" placeholder="비밀번호를 입력하세요" require />
+            <br />
+            <input type="password" className="checkpassword" placeholder="비밀번호를 입력하세요" require />
+            <br />
+            <input type="text" className="userid" placeholder="닉네임을 입력하세요" require />
+            <br />
+            <input type="text" className="userid" placeholder="이메일을 입력하세요" require />
+            <br />
+            <input type="text" className="userid" placeholder="핸드폰 번호를 입력하세요" require />
+            <br />
+            <input type="text" className="userid" placeholder="주소를 입력하세요" require />
+            <br />
+            <input type="text" className="userid" placeholder="세부 주소를 입력하세요" require />
+            <br />
+            <input type="text" className="userid" placeholder="카테고리를 선택하세요" require />
+            <br />
+            <button type="submit"></button>
+        </form>
     );
 };
 
