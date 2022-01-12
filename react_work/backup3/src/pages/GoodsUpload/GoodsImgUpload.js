42a663e3719acdda2228ada8c1d554165915820d
Neetbear <52730691+Neetbear@users.noreply.github.com>
2022-01-12 16:11:26 +0900

    0112

---
+++ react_work/backup3/src/pages/GoodsUpload/GoodsImgUpload.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { IoIosCamera } from 'react-icons/io';
+import { TiDelete } from 'react-icons/ti'
+
+function GoodsImgUpload(props) {
+  // const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleImgChange = (e) => {
+    if (e.target.files) {
+      const filesArray = Array.from(e.target.files).map((file) =>
+        URL.createObjectURL(file)
+      );
+
+      if (filesArray.length + props.selectedFiles.length <= 10) {
+
+        props.setSelectedFiles((prevImages) => prevImages.concat(filesArray));
+        Array.from(e.target.files).map(
+          (file) => URL.revokeObjectURL(file) // avoid memory leak
+        );
+      } else {
+        alert("10개 이하로 올려주세요")
+      }
+    }
+    console.log(props.selectedFiles);
+  };
+
+  const removeSelectedImage = (e) => {
+    console.log(e.target.length);
+  };
+
+  const renderPhotos = (source) => {
+    console.log("source: ", source);
+    return source.map((photo) => {
+      return (
+        <div>
+          <TiDelete className="deleteIcons"
+            onClick={removeSelectedImage}
+            id={source.indexOf(photo)}
+          />
+          <img src={photo} alt="" key={photo} />
+        </div>
+      );
+    });
+  };
+
+  return (
+      <div className="GoodsImgUploadBlock">
+        <div className="imgSelect">
+          <form action="/goodsupload" encType="multipart/form-data" method="post">
+            <input
+              type="file"
+              id="imgFile"
+              multiple
+              accept="image/*"
+              name="goods_img"
+              onChange={handleImgChange}
+            />
+            <label for="imgFile">
+              <IoIosCamera />
+              <div>{ props.selectedFiles.length } / 10</div>
+            </label>
+          </form>
+        </div>
+        <div className="imgResult">
+            {renderPhotos(props.selectedFiles)}
+        </div>
+      </div>
+  )
+}
+
+export default React.memo(GoodsImgUpload);
---
+++ react_work/backup3/src/pages/GoodsUpload/GoodsUpload copy.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import './GoodsUpload.css'
+import { CgClose } from 'react-icons/cg';
+import GoodsImgUpload from "./GoodsImgUpload";
+import { GrFormNext } from 'react-icons/gr';
+import { MdPostAdd } from 'react-icons/md';
+import { BsSliders } from 'react-icons/bs';
+import axios from "axios";
+
+function GoodsUpload() {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+
+  const onTitleHandler = (event) => {
+    setTitle(event.currentTarget.value)
+  }
+  const onPriceHandler = (event) => {
+    setPrice(event.currentTarget.value)
+  }
+  const onDescriptionHandler = (event) => {
+    setDescription(event.currentTarget.value)
+  }
+
+  const submitGoods = async() => {
+    axios.post('http://localhost:5001/api/goodsupload', {
+      title: title,
+      price: price,
+      description: description
+    }).then(()=> {
+      console.log(selectedFiles);
+      alert('등록 완료!');
+    })
+  }
+  
+
+  return (
+    <div className="GoodsUploadTemplate">
+      <div className="GoodsUploadHead">
+        <div><CgClose /></div>
+        <div>중고거래 글쓰기</div>
+        <div>
+          <button className="submit-button" onClick={submitGoods}>입력</button>
+        </div>
+      </div>
+      <GoodsImgUpload />
+      <div className="GoodsUploadTitle">
+        <input type="text" placeholder="글 제목" className="GoodsUploadTitleBox" value={title || ''} onChange={onTitleHandler} />
+      </div>
+      <div className="GoodsUploadCategory">
+        <div>카테고리 선택</div>
+        <GrFormNext />
+      </div>
+      <div className="GoodsUploadPrice">
+        <input type="text" placeholder="₩ 가격 (선택사항)" className="GoodsUploadPriceBox" value={price || ''} onChange={onPriceHandler} />
+        <div>
+          <input type="checkbox" id="goodsPrice" />
+          <label >가격 제안받기</label>
+        </div>
+      </div>
+      <div className="GoodsUploadContents">
+        <input type="textarea" id="goodsContents" placeholder="000동에 올릴 게시글 내용을 작성해주세요.(가품 및 판매 금지품목은 게시가 제한될 수 있어요.)" value={description || ''} onChange={onDescriptionHandler} />
+      </div>
+      <div className="GoodsUploadFooter">
+        <div><MdPostAdd />자주 쓰는 문구 </div>
+        <div><BsSliders /> 보여줄 동네 설정</div>
+      </div>
+    </div>
+  )
+}
+
+export default GoodsUpload;
